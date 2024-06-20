0e4898808568a38c01929378e62db0f55465ca32
rajanhere <rajan@brainstackhub.com.au>
2024-06-20 20:42:04 +1000

    quiz app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './Components/QuizPage/Quiz'
+import Quiz from './Components/QuizPage/Quiz'
+function App() {
+
+  return (
+    <>
+<Quiz />
+
+
+    </>
+  )
+}
+
+export default App
